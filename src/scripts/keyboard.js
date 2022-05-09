2e00a204b8448a663e348b74ae13bc73e2591fdb
Glanya <sokolovskayaglafira@gmail.com>
2022-05-09 19:37:47 +0300

    feat: add capslock

--- src/scripts/keyboard.js
+++ src/scripts/keyboard.js
@@ -1,11 +1,12 @@
 import keys from './keys.js';
-import { createElement } from './app.js';
+import { createElement } from './index.js';
 
 class Keyboard {
   constructor() {
-    this.capslock = 'off';
-    this.shift = 'off';
+    this.capslock = false;
+    this.shift = false;
     this.language = 'eng';
+    this.textarea = document.querySelector('textarea');
   }
 
   createKeyboard() {
@@ -21,7 +22,7 @@ class Keyboard {
           charLang = item.keyChar;
         }
         const key = createElement('div', charLang, 'key');
-        key.dataset.keyCode = item.keyCode;
+        key.dataset.code = item.keyCode;
         if (item.keyChar.ru && item.keyChar.eng) {
           key.dataset.ru = item.keyChar.ru;
           key.dataset.eng = item.keyChar.eng;
@@ -42,6 +43,32 @@ class Keyboard {
     }
     keyboard.append(keyboardWrapper);
     return keyboard;
+  }
+  
+  pressCapsLock() {
+    if (this.capslock === true) {
+      this.capslock = false;
+    } else {
+      this.capslock = true;
+    }
+
+    document.querySelectorAll('.key').forEach((e) => {
+      if (e.dataset[this.language]) {
+        if (this.capslock === true) {
+          e.innerHTML = e.dataset[this.language].toUpperCase();
+        } else {
+          e.innerHTML = e.dataset[this.language];
+        }
+      }
+    });
+  }
+
+  pressKey(e, key) {
+    e.preventDefault();
+    const keyName = key.dataset.code;
+    if (keyName === 'CapsLock') {
+      this.pressCapsLock();
+    }
   }
 }
 
