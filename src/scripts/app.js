4590a26431969599faa5b6b8e31b204708a27db9
Glanya <sokolovskayaglafira@gmail.com>
2022-05-09 19:37:34 +0300

    feat: add mouse interaction

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,7 @@
 import { createElement } from './index.js';
-import { Keyboard, pressKey } from './keyboard.js';
+import { Keyboard } from './keyboard.js';
+
+const keyboard = new Keyboard();
 
 const createPage = () => {
   const body = document.querySelector('body');
@@ -8,7 +10,7 @@ const createPage = () => {
   const language = 'Для переключения языка комбинация: левыe ctrl + alt';
 
   const wrapper = createElement('div', '', 'wrapper');
-  const keyboard = new Keyboard();
+
   wrapper.append(createElement('h1', title, 'title'));
   wrapper.append(createElement('textarea', '', 'textarea'));
   wrapper.append(keyboard.createKeyboard());
@@ -22,10 +24,25 @@ createPage();
 document.addEventListener('keydown', (e) => {
   const keyBtn = document.querySelector(`[data-code=${e.code}]`);
   keyBtn.classList.add('active');
-  pressKey(e);
+  keyboard.pressKey(e, keyBtn);
 });
 
 document.addEventListener('keyup', (e) => {
   const keyBtn = document.querySelector(`[data-code=${e.code}]`);
   keyBtn.classList.remove('active');
+});
+
+document.querySelector('.keyboard').addEventListener('mousedown', (e) => {
+  if (e.target.closest('.key')) {
+    const keyBtn = e.target.closest('.key');
+    keyBtn.classList.add('active');
+    keyboard.pressKey(e, keyBtn);
+  }
+});
+
+document.querySelector('.keyboard').addEventListener('mouseup', (e) => {
+  if (e.target.closest('.key')) {
+    const keyBtn = e.target.closest('.key');
+    keyBtn.classList.remove('active');
+  }
 });
